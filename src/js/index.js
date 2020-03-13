ffbce56af7daa0811f6548de947cffb249bcd625
olivercastroteran <olivercastroteran@gmail.com>
2020-03-12 21:26:27 -0400

    localStorage implemented

--- src/js/index.js
+++ src/js/index.js
@@ -141,9 +141,6 @@ elements.shopping.addEventListener('click', e => {
 });
 
 /* ********************************* LIKES CONTROLLER ********************************* */
-// TESTING
-state.likes = new Likes();
-likesView.toggleLikeMenu(state.likes.getNumLikes());
 
 const controlLike = () => {
   if (!state.likes) state.likes = new Likes();
@@ -178,6 +175,17 @@ const controlLike = () => {
   }
   likesView.toggleLikeMenu(state.likes.getNumLikes());
 };
+
+// Restore liked recipes on page load
+window.addEventListener('load', () => {
+  state.likes = new Likes();
+  // Restore likes
+  state.likes.readStorage();
+  // Toggle like menu btn
+  likesView.toggleLikeMenu(state.likes.getNumLikes());
+  // Render the existing likes
+  state.likes.likes.forEach(like => likesView.renderLike(like));
+});
 
 /* ********************************* EVENT LISTENERS ********************************* */
 
